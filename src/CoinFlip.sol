55805910215d306362465aa02ab0700b29a4e865
cooptothe <edward.cooper97@gmail.com>
2023-11-08 10:04:54 -0600

    finished coinflip function

--- src/CoinFlip.sol
+++ src/CoinFlip.sol
@@ -16,4 +16,14 @@ contract CoinFlip {
         CoinSide chosenSide,
         FlipResult result
     );
+
+    function flipCoin(CoinSide chosenSide) public {
+        uint256 randomNumber = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender))) % 2;
+
+        CoinSide result = CoinSide(randomNumber);
+
+        FlipResult flipResult = (chosenSide == result) ? FlipResult.WIN : FlipResult.LOSE;
+
+        emit Result(msg.sender, chosenSide, flipResult);
+    }
 }
