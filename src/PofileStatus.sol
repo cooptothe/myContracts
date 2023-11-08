d7952c303b6d4f0771dafa6fc9bb29e192f5b6c4
cooptothe <edward.cooper97@gmail.com>
2023-11-08 11:23:34 -0600

    added and completed functionality of the profile status contract

--- src/PofileStatus.sol
+++ src/PofileStatus.sol
@@ -6,4 +6,33 @@ contract ProfileStatus {
         string statusMessage;
         bool exists;
     }
+
+    mapping(address => Status) public userStatus;
+
+    event StatusCreated(address indexed wallet, string status);
+    event StatusUpdated(address indexed wallet, string newStatus);
+
+    function createStatus(string memory initialStatus) public {
+        require(!userStatus[msg.sender].exists, "Status already exists.");
+        userStatus[msg.sender] = Status({
+            statusMessage: initialStatus,
+            exists: true
+        });
+
+        emit StatusCreated(msg.sender, initialStatus);
+    }
+
+    function updateStatus(string memory newStatus) public {
+        require(userStatus[msg.sender].exists, "Status doesn't exist.");
+
+        userStatus[msg.sender].statusMessage = newStatus;
+
+        emit StatusUpdated(msg.sender, newStatus);
+    }
+
+    function getStatus(address wallet) public view returns (string memory) {
+        require(userStatus[wallet].exists, "Status doesn't exist.");
+
+        return userStatus[wallet].statusMessage;
+    }
 }
