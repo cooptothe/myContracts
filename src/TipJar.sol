1b4f94f262031a594b0d16e35812fb7672276b35
cooptothe <edward.cooper97@gmail.com>
2023-11-08 10:23:16 -0600

    added address and evnts owner reciever and amounts

--- src/TipJar.sol
+++ src/TipJar.sol
@@ -2,5 +2,12 @@
 pragma solidity ^0.8.13;
 
 contract TipJar {
-    
+    address public owner;
+
+    event TipRecieved(address indexed tipper, uint256 amount);
+    event TipWithdrawn(address indexed owner, uint256 amount);
+
+    constructor() {
+        
+    }
 }
